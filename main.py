d1ababc22e68f757c55e26a8610f540957599f2e
Nina Sproule <qut371@usask.ca>
2025-06-24 14:53:57 -0600

    add test madlib template

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-#hi
-#testing git
+
+
+
+template = "The boys can watch an hour of <adjective> television before turning off the <plural noun> in their room. Make sure they do not watch any violent <plural noun> or adult <plural noun>. If there are any phone <plural noun>, do not identify yourself as the <noun>-sitter. Take a message. Write it <adverb> on the <noun> provided."
+
