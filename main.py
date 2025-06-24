bd29b21752ad9787d8a5872d9994697a267100a8
Nina Sproule <qut371@usask.ca>
2025-06-24 15:19:17 -0600

    documentation

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+#Nina Sproule 2025
 
 template = "The boys can watch an hour of <adjective> television before turning off the <pluralnoun> in their room. Make sure they do not watch any violent <pluralnoun> or adult <pluralnoun>. If there are any phone <pluralnoun>, do not identify yourself as the <noun>-sitter. Take a message. Write it <adverb> on the <noun> provided."
-
 
 madlib = ""
 
 for word in template.split():
     if word.startswith("<"):
-        user_word = input("Enter a(n) " + word[1:word.find(">")]+ ". ")
+        user_word = input("Give me a(n) " + word[1:word.find(">")]+ ". ")
         madlib += user_word
         if word.find(">") < len(word):
-            madlib += word[word.find(">")+1:]
+            madlib += word[word.find(">")+1:] #for  punctuation and such immediately after the input word
         madlib += " "
     else:
         madlib += word
