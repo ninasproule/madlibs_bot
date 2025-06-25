8872daf31e97645f55f7d6a17cfc7f6b339bdfdd
Nina Sproule <qut371@usask.ca>
2025-06-25 14:00:25 -0600

    working with commands

--- bot.py
+++ bot.py
@@ -1,51 +1,32 @@
 # python bot.py
 import os
 import discord
-import random
-
 from discord.ext import commands
 from dotenv import load_dotenv
 
-
-
 intents = discord.Intents.default()
 intents.message_content = True
-#intents.all = True
 
-client = discord.Client(intents=intents)
-
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
 
-    if message.content.startswith('ping'):
-        await message.channel.send('Pong!')
+"""@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)"""
 
-"""bot = commands.Bot(intents=intents,command_prefix='!')
+@bot.command()
+async def new(ctx):
+    await ctx.send("To make your own madlib, type a story. Wherever you want the player to enter a word, put the type of word inside <>, like <NOUN> or <ADJECTIVE>.")
 
-@bot.command(name='99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
 
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-
-"""
-client.run(TOKEN)
+bot.run(TOKEN)
