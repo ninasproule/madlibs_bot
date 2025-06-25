c28baf686be304802bde25432eaf470928c5bdf6
Nina Sproule <qut371@usask.ca>
2025-06-25 14:29:26 -0600

    it adds reactions!

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from discord.interactions import InteractionResponse
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,13 +21,27 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong!')
 
-"""@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)"""
+
+
+"""def num_emoji_check(reaction, user):
+    return str(reaction.emoji) in ['1️⃣', '2️⃣'] and user != bot.user
+"""
+
 
 @bot.command()
 async def new(ctx):
     await ctx.send("To make your own madlib, type a story. Wherever you want the player to enter a word, put the type of word inside <>, like <NOUN> or <ADJECTIVE>.")
+    message = await ctx.send("Select 1 to make your own madlib, Select 2 to see an example.")
+    await message.add_reaction("1️⃣")
+    await message.add_reaction("2️⃣")
+
+#1️⃣
+# 2️⃣
+#def check(r, u):
+        #return str(r.emoji) == str(emoji) and r.message.id == message.id
+
+   # reaction, user = await bot.wait_for('reaction_add', timeout=60, check=check)
+
 
 
 bot.run(TOKEN)
