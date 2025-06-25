a1685e7d01d67ff7de975303ba154df082ca0cd4
Nina Sproule <qut371@usask.ca>
2025-06-25 15:57:34 -0600

    madlib creation but not saving

--- bot.py
+++ bot.py
@@ -1,9 +1,33 @@
 # python bot.py
 import os
 import discord
+import asyncio
+import json
 from discord.ext import commands
 from dotenv import load_dotenv
 from discord.interactions import InteractionResponse
+
+"""the_babysitter = "The boys can watch an hour of <adjective> television before turning off the <plural-noun> in their room. Make sure they do not watch any violent <plural-noun> or adult <plural-noun>. If there are any phone <plural-noun>, do not identify yourself as the <noun>-sitter. Take a message. Write it <adverb> on the <noun> provided."
+the_miner = "Once upon a time, a miner named Thabo worked in a big <PLACE>. Every day, he woke up early to <VERB> for shiny diamonds deep in the ground. Thabo’s helmet was <ADJECTIVE>, and his boots were covered in dirt. He used his pickaxe to <VERB> through the tough rock, <CONJUNCTION> he never gave up. One day, Thabo found a diamond that sparkled so brightly it made the <PLACE> look magical. He was excited <CONJUNCTION> a little nervous because it was the biggest diamond he had ever seen. He placed the diamond in his bag <ADVERB> so it wouldn’t get scratched. When Thabo returned to the surface, he shared the news with his team, <CONJUNCTION> everyone cheered. They knew their hard work had paid off, and Thabo felt <ADJECTIVE> as he walked home."
+wedding_vows = "I, <MOVIE-CHARACTER-NAME>, choose you, <TV-CHARACTER-NAME>, to be my spouse for life. Together, we’ll face the ups and <PLURAL-DIRECTION>, always by each other’s side. I offer you my <BODY-PART> and <BODY-ORGAN> as a safe haven filled with love and <NOUN>. I promise to stay <ADJECTIVE> and devoted to you. Like this never-ending <SHAPE>, my love for you will endure <AMOUNT-OF-TIME>. Just as this ring is made of <ADJECTIVE> material, my commitment to you will never <VERB>. With this ring, I <VERB> you."
+
+templates = {"The Babysitter":the_babysitter,
+             "The Miner":the_miner,
+             "Wedding Vows":wedding_vows}
+def jsonWrite():
+    with open("madlib_temps.json", mode="w", encoding="utf-8") as write_file:
+        json.dump(templates, write_file)
+
+def jsonRead():
+    with open("madlib_temps.json", mode="r", encoding="utf-8") as read_file:
+        saved_madlibs = json.load(read_file)
+    return saved_madlibs
+
+
+templates = jsonRead()
+"""
+
+templates = {}
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,24 +47,46 @@ async def ping(ctx):
 
 
 
-"""def num_emoji_check(reaction, user):
-    return str(reaction.emoji) in ['1️⃣', '2️⃣'] and user != bot.user
-"""
-
 
 @bot.command()
 async def new(ctx):
+    def check(m):
+        return m.content is not None and m.author == ctx.author
+
     await ctx.send("To make your own madlib, type a story. Wherever you want the player to enter a word, put the type of word inside <>, like <NOUN> or <ADJECTIVE>.")
-    message = await ctx.send("Select 1 to make your own madlib, Select 2 to see an example.")
-    await message.add_reaction("1️⃣")
-    await message.add_reaction("2️⃣")
+    await ctx.send("Type 1 to make your own madlib, Type 2 to see an example.")
+
+    message = await bot.wait_for('message')#, check=check())
+
+    if message.content.startswith("1"):  # make new madlib
+        await message.channel.send("Go ahead and type your madlib! All in one message, please.")
+        madlib = await bot.wait_for('message')#, check=check)
+        new_template = str(madlib.content)
+
+        #await ctx.send("Here is your madlib:\n" + madlib + "\nIs this correct?")
+        #confirm = await bot.wait_for('message', check=check())
+
+        await ctx.send("What would you like to title this madlib?")
+        title = await bot.wait_for('message')#, check=check())
+        new_title = str(title.content)
+
+        await ctx.send("Madlib registered!")
+
+        templates[new_title] = new_template
+        #jsonWrite()
+        print(templates)
+
+
+
+
+    #await message.add_reaction("1️⃣")
+    #await message.add_reaction("2️⃣")
+
+
 
 #1️⃣
 # 2️⃣
-#def check(r, u):
-        #return str(r.emoji) == str(emoji) and r.message.id == message.id
 
-   # reaction, user = await bot.wait_for('reaction_add', timeout=60, check=check)
 
 
 
