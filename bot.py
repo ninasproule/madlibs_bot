2dfcff2525403bcda6f036c62c781a4cdfedd163
Nina Sproule <qut371@usask.ca>
2025-06-25 13:07:23 -0600

    ping! pong!
    it's aliiiive

--- bot.py
+++ bot.py
@@ -1,15 +1,51 @@
 # python bot.py
 import os
 import discord
+import random
+
+from discord.ext import commands
 from dotenv import load_dotenv
+
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+#intents.all = True
+
+client = discord.Client(intents=intents)
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('ping'):
+        await message.channel.send('Pong!')
+
+"""bot = commands.Bot(intents=intents,command_prefix='!')
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+"""
 client.run(TOKEN)
