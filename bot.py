e20998042b54aeb4302db72d9be5cc69ba12b7b9
Nina Sproule <qut371@usask.ca>
2025-06-25 16:08:51 -0600

    saving to json yippee!

--- bot.py
+++ bot.py
@@ -7,13 +7,14 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from discord.interactions import InteractionResponse
 
-"""the_babysitter = "The boys can watch an hour of <adjective> television before turning off the <plural-noun> in their room. Make sure they do not watch any violent <plural-noun> or adult <plural-noun>. If there are any phone <plural-noun>, do not identify yourself as the <noun>-sitter. Take a message. Write it <adverb> on the <noun> provided."
+the_babysitter = "The boys can watch an hour of <adjective> television before turning off the <plural-noun> in their room. Make sure they do not watch any violent <plural-noun> or adult <plural-noun>. If there are any phone <plural-noun>, do not identify yourself as the <noun>-sitter. Take a message. Write it <adverb> on the <noun> provided."
 the_miner = "Once upon a time, a miner named Thabo worked in a big <PLACE>. Every day, he woke up early to <VERB> for shiny diamonds deep in the ground. Thabo’s helmet was <ADJECTIVE>, and his boots were covered in dirt. He used his pickaxe to <VERB> through the tough rock, <CONJUNCTION> he never gave up. One day, Thabo found a diamond that sparkled so brightly it made the <PLACE> look magical. He was excited <CONJUNCTION> a little nervous because it was the biggest diamond he had ever seen. He placed the diamond in his bag <ADVERB> so it wouldn’t get scratched. When Thabo returned to the surface, he shared the news with his team, <CONJUNCTION> everyone cheered. They knew their hard work had paid off, and Thabo felt <ADJECTIVE> as he walked home."
 wedding_vows = "I, <MOVIE-CHARACTER-NAME>, choose you, <TV-CHARACTER-NAME>, to be my spouse for life. Together, we’ll face the ups and <PLURAL-DIRECTION>, always by each other’s side. I offer you my <BODY-PART> and <BODY-ORGAN> as a safe haven filled with love and <NOUN>. I promise to stay <ADJECTIVE> and devoted to you. Like this never-ending <SHAPE>, my love for you will endure <AMOUNT-OF-TIME>. Just as this ring is made of <ADJECTIVE> material, my commitment to you will never <VERB>. With this ring, I <VERB> you."
 
 templates = {"The Babysitter":the_babysitter,
              "The Miner":the_miner,
              "Wedding Vows":wedding_vows}
+
 def jsonWrite():
     with open("madlib_temps.json", mode="w", encoding="utf-8") as write_file:
         json.dump(templates, write_file)
@@ -25,9 +26,7 @@ def jsonRead():
 
 
 templates = jsonRead()
-"""
 
-templates = {}
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -40,6 +39,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    print(templates.keys())
 
 @bot.command()
 async def ping(ctx):
@@ -63,7 +63,7 @@ async def new(ctx):
         madlib = await bot.wait_for('message')#, check=check)
         new_template = str(madlib.content)
 
-        #await ctx.send("Here is your madlib:\n" + madlib + "\nIs this correct?")
+        #await ctx.send("Here is your madlib:\n" + madlib.content + "\nIs this correct?")
         #confirm = await bot.wait_for('message', check=check())
 
         await ctx.send("What would you like to title this madlib?")
@@ -73,16 +73,13 @@ async def new(ctx):
         await ctx.send("Madlib registered!")
 
         templates[new_title] = new_template
-        #jsonWrite()
-        print(templates)
-
+        jsonWrite()
+        print(templates.keys())
 
 
 
     #await message.add_reaction("1️⃣")
     #await message.add_reaction("2️⃣")
-
-
 
 #1️⃣
 # 2️⃣
