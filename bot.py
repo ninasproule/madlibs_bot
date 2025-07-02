2d771bd3152cf0ce6e613d6ea700887fb34cfbce
Nina Sproule <qut371@usask.ca>
2025-07-02 12:59:08 -0600

    allow bot dm interactions

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ TITLES_PER_PAGE = 8
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
 
 
